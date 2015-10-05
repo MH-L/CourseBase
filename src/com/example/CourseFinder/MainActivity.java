d8579b063c17b22d0a4723296a515ea56ad2043e
MH-L <922lmh922@gmail.com>
2015-10-05 11:48:57 +0800

    Second Commit.

---
+++ src/com/example/CourseFinder/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.CourseFinder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Minghao on 10/5/2015.
+ */
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        this.setContentView(R.layout.main);
+        super.onCreate(savedInstanceState);
+    }
+}
